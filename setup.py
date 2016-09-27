266d6f02a0c3fd8cc3999d1eef53726583c9404c
steph papanik <spapanik21@gmail.com>
2016-09-27 18:09:54 +0100

    update setup format

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+PKG_NAME = 'pyrencode'
+__version__ = '0.0.4'
+
 
 def listify(filename):
     with open(filename) as f:
@@ -11,16 +14,19 @@ def contents(filename):
         return f.read()
 
 setup(
-    name='pyrencode',
-    packages=['pyrencode', 'pyrencode.settings'],
-    version='0.0.4',
+    name=PKG_NAME,
+    packages=[PKG_NAME, 'pyrencode.settings'],
+    version=__version__,
     description='A pure python rencoder',
     license='MIT',
     long_description=contents('README.txt'),
     author='Stephanos Papanikolopoulos',
     author_email='spapanik21@gmail.com',
-    url='https://github.com/spapanik/pyrencode',
-    download_url='https://github.com/spapanik/pyrencode/tarball/0.0.4',
+    url='https://github.com/spapanik/{pkg_name}'.format(pkg_name=PKG_NAME),
+    download_url='https://github.com/spapanik/{pkg_name}/tarball/{ver}'.format(
+        pkg_name=PKG_NAME,
+        ver=__version__,
+    ),
     keywords=listify('KEYWORDS.txt'),
     classifiers=listify('CLASSIFIERS.txt'),
 )
