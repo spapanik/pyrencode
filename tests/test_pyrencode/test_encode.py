6a815c39a6e96241100832990bca962a1d467cf1
Stephanos Kuma <stephanos.papanikolopoulos@oaknorth.ai>
2019-12-03 16:38:00 +0000

    Add tests for encoding

---
+++ tests/test_pyrencode/test_encode.py
@@ -0,0 +1,15 @@
+import pytest
+
+from pyrencode import encode
+
+
+def test_encode_int_under_max_size():
+    encode.encode_int(2 ** 128, [])
+
+
+def test_encode_int_over_max_size():
+    pytest.raises(OverflowError, encode.encode_int, 2 ** 256, [])
+
+
+def test_dumps_wrong_float_bits():
+    pytest.raises(ValueError, encode.dumps, None, float_bits=128)
