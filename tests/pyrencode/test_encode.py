64325b68582158b7197ad9f9b81676e33a50474e
Stephanos Kuma <stephanos@kuma.ai>
2022-02-09 16:42:26 +0000

    Run stable version of black

--- tests/pyrencode/test_encode.py
+++ tests/pyrencode/test_encode.py
@@ -4,11 +4,11 @@ from pyrencode import encode
 
 
 def test_encode_int_under_max_size():
-    encode.encode_int(2 ** 128, [])
+    encode.encode_int(2**128, [])
 
 
 def test_encode_int_over_max_size():
-    pytest.raises(OverflowError, encode.encode_int, 2 ** 256, [])
+    pytest.raises(OverflowError, encode.encode_int, 2**256, [])
 
 
 def test_dumps_wrong_float_bits():
