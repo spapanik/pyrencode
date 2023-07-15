4e40eed54331d24befeb3ba985309abf2c6d173c
Stephanos Kuma <stephanos@kuma.ai>
2023-07-15 11:52:02 +0100

    Test for exceptions with context manager

--- tests/pyrencode/test_encode.py
+++ tests/pyrencode/test_encode.py
@@ -8,8 +8,10 @@ def test_encode_int_under_max_size() -> None:
 
 
 def test_encode_int_over_max_size() -> None:
-    pytest.raises(OverflowError, encode.encode_int, 2**256, [])
+    with pytest.raises(OverflowError):
+        encode.encode_int(2**256, [])
 
 
 def test_dumps_wrong_float_bits() -> None:
-    pytest.raises(ValueError, encode.dumps, None, float_bits=128)
+    with pytest.raises(ValueError):
+        encode.dumps(None, float_bits=128)
